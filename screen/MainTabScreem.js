746f293ff30096753c757e82e854c6c1f454fe43
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-12-12 14:59:16 +0100

    reorganize files

--- screen/MainTabScreem.js
+++ screen/MainTabScreem.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
-import IonIcon from 'react-native-vector-icons/Ionicons';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './HomeScreen';
 import DetailsScreen from './DetailsScreen';
-import ProfileScreen from './ProfileScreen';
 import ExploreScreen from './ExploreScreen';
+import ProfileScreen from './ProfileScreen';
 
 const HomeStack = createStackNavigator();
 const DetailsStack = createStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
 
 const HomeStackScreen = ({navigation}) => (
   <HomeStack.Navigator
@@ -28,13 +31,11 @@ const HomeStackScreen = ({navigation}) => (
       options={{
         title: 'Overview',
         headerLeft: () => (
-          <IonIcon.Button
+          <Icon.Button
             name="menu"
-            backgroundColor="#009387"
             size={25}
-            onPress={() => {
-              navigation.openDrawer();
-            }}
+            backgroundColor="#009387"
+            onPress={() => navigation.openDrawer()}
           />
         ),
       }}
@@ -42,7 +43,7 @@ const HomeStackScreen = ({navigation}) => (
   </HomeStack.Navigator>
 );
 
-const DetaislStackScreen = ({navigation}) => (
+const DetailsStackScreen = ({navigation}) => (
   <DetailsStack.Navigator
     screenOptions={{
       headerStyle: {
@@ -58,13 +59,11 @@ const DetaislStackScreen = ({navigation}) => (
       component={DetailsScreen}
       options={{
         headerLeft: () => (
-          <IonIcon.Button
+          <Icon.Button
             name="menu"
-            backgroundColor="#1f65ff"
             size={25}
-            onPress={() => {
-              navigation.openDrawer();
-            }}
+            backgroundColor="#1f65ff"
+            onPress={() => navigation.openDrawer()}
           />
         ),
       }}
@@ -72,57 +71,49 @@ const DetaislStackScreen = ({navigation}) => (
   </DetailsStack.Navigator>
 );
 
-const Tab = createMaterialBottomTabNavigator();
-
-const MainTabScreen = () => {
-  return (
-    <Tab.Navigator initialRouteName="Home" activeColor="#fff">
-      <Tab.Screen
-        name="Home"
-        component={HomeStackScreen}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarColor: '#009387',
-          tabBarIcon: ({color}) => (
-            <IonIcon name="home" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={DetaislStackScreen}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarColor: '#1f65ff',
-          tabBarIcon: ({color}) => (
-            <IonIcon name="notifications" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarColor: '#694fad',
-          tabBarIcon: ({color}) => (
-            <IonIcon name="person" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Explore"
-        component={ExploreScreen}
-        options={{
-          tabBarLabel: 'Explore',
-          tabBarColor: '#d02860',
-          tabBarIcon: ({color}) => (
-            <IonIcon name="aperture" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-};
+const MainTabScreen = () => (
+  <Tab.Navigator initialRouteName="Home" activeColor="#fff">
+    <Tab.Screen
+      name="Home"
+      component={HomeStackScreen}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarColor: '#009387',
+        tabBarIcon: ({color}) => <Icon name="home" color={color} size={26} />,
+      }}
+    />
+    <Tab.Screen
+      name="Notifications"
+      component={DetailsStackScreen}
+      options={{
+        tabBarLabel: 'Updates',
+        tabBarColor: '#1f65ff',
+        tabBarIcon: ({color}) => (
+          <Icon name="notifications" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        tabBarLabel: 'Profile',
+        tabBarColor: '#694fad',
+        tabBarIcon: ({color}) => <Icon name="person" color={color} size={26} />,
+      }}
+    />
+    <Tab.Screen
+      name="Explore"
+      component={ExploreScreen}
+      options={{
+        tabBarLabel: 'Explore',
+        tabBarColor: '#d02860',
+        tabBarIcon: ({color}) => (
+          <Icon name="aperture" color={color} size={26} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
+);
 
 export default MainTabScreen;
