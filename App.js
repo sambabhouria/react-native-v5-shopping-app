cefa0f462e7e441439d4ceda0e512eed70fcce78
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-12-13 02:07:47 +0100

    Custom Drawer Navigator in React Navigation v5

--- App.js
+++ App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
-import MainTabScreen from './screen/MainTabScreem';
+import MainTabScreen from './screens/MainTabScreem';
+import SupportScreen from './screens/SupportScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import BookmarkScreen from './screens/BookmarkScreen';
+
+import DrawerContent from './screens/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={MainTabScreen} />
-        {/* <Drawer.Screen name="Details" component={DetaislStackScreen} /> */}
+      <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
+        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
       </Drawer.Navigator>
       {/* */}
     </NavigationContainer>
